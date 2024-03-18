46a6b55782c549e5786e3fbf1437d5d17d8a2ef2
Arsen Kozariv <arik912@gmail.com>
2024-03-18 18:57:18 +0100

    fix

--- HT03/task3.js
+++ HT03/task3.js
@@ -8,23 +8,20 @@ const csvReadStream = fs.createReadStream(csvFilePath, { encoding: 'utf8' });
 
 const txtWriteStream = fs.createWriteStream(txtFilePath, { encoding: 'utf8' });
 
-// Convert CSV to JSON and write to TXT file line by line
 csvReadStream
   .pipe(csvtojson())
   .on('data', (jsonObj) => {
-    console.log(jsonObj.toString())
-    const jsonStr = JSON.stringify(jsonObj) + '\n'; // Convert JSON object to string
-    txtWriteStream.write(jsonStr); // Write JSON string to TXT file
+    const jsonStr = JSON.stringify(jsonObj.toString()) + '\n'; // ask on QA session about buffer to string ???
+    txtWriteStream.write(jsonStr); 
   })
   .on('error', (err) => {
     console.error('Error reading CSV file:', err);
   })
   .on('end', () => {
     console.log('CSV to TXT conversion completed.');
-    txtWriteStream.end(); // Close the writable stream
+    txtWriteStream.end(); 
   });
 
-// Log any errors that occur during writing to the TXT file
 txtWriteStream.on('error', (err) => {
   console.error('Error writing to TXT file:', err);
 });
