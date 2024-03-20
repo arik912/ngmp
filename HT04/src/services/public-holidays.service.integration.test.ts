3e90d5035ca93ba14a787d4b8f652dacbfa475cd
Arsen Kozariv <arik912@gmail.com>
2024-03-20 12:43:10 +0100

    add integrations test

--- HT04/src/services/public-holidays.service.integration.test.ts
+++ HT04/src/services/public-holidays.service.integration.test.ts
@@ -1,10 +1,12 @@
 import request from 'supertest';
 
+const host = 'https://date.nager.at/api/v3'
+
 describe('End-to-End Tests', () => {
   it('should return list of public holidays for a specific country and year', async () => {
     const year = 2024;
     const country = 'GB';
-    const response = await request('https://date.nager.at')
+    const response = await request(host)
       .get(`/PublicHolidays/${year}/${country}`)
       .set('Accept', 'application/json');
     
@@ -14,17 +16,16 @@ describe('End-to-End Tests', () => {
 
   it('should check if today is public holiday in a specific country', async () => {
     const country = 'GB';
-    const response = await request('https://date.nager.at')
+    const response = await request(host)
       .get(`/IsTodayPublicHoliday/${country}`)
       .set('Accept', 'application/json');
     
-    expect(response.status).toBe(200);
-    expect(response.body).toHaveProperty('isPublicHoliday');
+    expect(response.status).toBe(204);
   });
 
   it('should return next public holidays for a specific country', async () => {
     const country = 'GB';
-    const response = await request('https://date.nager.at')
+    const response = await request(host)
       .get(`/NextPublicHolidays/${country}`)
       .set('Accept', 'application/json');
     
